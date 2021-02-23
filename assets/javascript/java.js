4480e098bfbda478f2650c8959c667f884d0de9d
Tom Barber <trb5160@gmail.com>
2021-02-23 18:54:08 -0500

    updated screenshot in readme

--- assets/javascript/java.js
+++
@@ -1,7 +0,0 @@
-$('#navbar ul li a').on('click',function(event){
-    var $anchor = $(this);
-    $('html, body').animate({
-      scrollTop: $($anchor.attr('href')).offset().top + "px"
-    }, 1500);
-    event.preventDefault();
- });
